f689dd1a8b3396903e059883e6e47616f1ba831c
Laura Flenley <laura.flenley@worc.ox.ac.uk>
2023-11-07 22:31:03 +0000

    This set of files contains analysis of dataset 1 of logistic growth experiment included in the Github practical on 7/11/23

--- fit_linear_model.R
+++ fit_linear_model.R
@@ -1,19 +1,27 @@
 #Script to estimate the model parameters using a linear approximation
+install.packages("dplyr")
+library(dplyr)
 
-#library(dplyr)
-
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment1.csv")
 
 #Case 1. K >> N0, t is small
 
-data_subset1 <- growth_data %>% filter(t<???) %>% mutate(N_log = log(N))
+data_subset1 <- growth_data %>% filter(t<1700) %>% mutate(N_log = log(N))
 
 model1 <- lm(N_log ~ t, data_subset1)
 summary(model1)
+#so intercept of the model is 6.927 = log(N0); r = 0.0099
+#so N0 = exp(6.927)
 
 #Case 2. N(t) = K
 
-data_subset2 <- growth_data %>% filter(t>???)
+data_subset2 <- growth_data %>% filter(t>3000)
 
 model2 <- lm(N ~ 1, data_subset2)
 summary(model2)
+#so k = intercept = 6e+10
+
+
+sink(file = "package-versions.txt")
+sessionInfo()
+sink()
--- plot_data.R
+++ plot_data.R
@@ -1,11 +1,12 @@
 #Script to plot the logistic growth data
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment1.csv")
 
 install.packages("ggplot2")
 library(ggplot2)
 
-ggplot(aes(t,N), data = ???) +
+#plot graph of the data
+ggplot(aes(t,N), data = growth_data) +
   
   geom_point() +
   
@@ -15,7 +16,8 @@ ggplot(aes(t,N), data = ???) +
   
   theme_bw()
 
-ggplot(aes(t,???), data = growth_data) +
+#plot graph of the log transformed data
+ggplot(aes(t,N), data = growth_data) +
   
   geom_point() +
   
